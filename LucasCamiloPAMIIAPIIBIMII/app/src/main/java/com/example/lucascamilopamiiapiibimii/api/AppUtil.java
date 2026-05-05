5ed60fdab81d4ff4482984f9f33a1fd742e0e1e4
Prosopagnosatico <168598761+Prosopagnosatico@users.noreply.github.com>
2026-05-05 11:22:42 -0300

    Add files via upload

---
+++ LucasCamiloPAMIIAPIIBIMII/app/src/main/java/com/example/lucascamilopamiiapiibimii/api/AppUtil.java
@@ -0,0 +1,5 @@
+package com.example.lucascamilopamiiapiibimii.api;
+
+public class AppUtil {
+    public static final String TAG = "lucas";
+}
---
+++ LucasCamiloPAMIIAPIIBIMII/app/src/main/java/com/example/lucascamilopamiiapiibimii/controller/ClienteController.java
@@ -0,0 +1,11 @@
+package com.example.lucascamilopamiiapiibimii.controller;
+
+import android.content.Context;
+import com.example.lucascamilopamiiapiibimii.datasource.AppDataBase;
+
+public class ClienteController extends AppDataBase {
+
+    public ClienteController(Context context){
+        super(context);
+    }
+}
